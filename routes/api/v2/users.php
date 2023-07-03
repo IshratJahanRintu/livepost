5f040682069e0b6367f0e6f26c85f025c6c02feb
IshratJahanRintu <ishrat2415@student.nstu.edu.bd>
2023-07-03 15:14:18 +0600

    version 2 added

---
+++ routes/api/v2/users.php
@@ -0,0 +1,30 @@
+
+<?php
+
+use App\Http\Controllers\UserController;
+use GuzzleHttp\Middleware;
+use Illuminate\Support\Facades\Route;
+
+Route::group([
+    // "middleware" => ['auth'],
+    "as" => 'users.',
+
+    'namespace' => "\App\Http\Controllers"
+], function () {
+
+    Route::get('/users', 'UserController@v2index')->name('index');
+    Route::get("/users/{user}", 'UserController@show')->name('show');
+    Route::post("/users", [UserController::class, 'store'])->name('store');
+    Route::patch("/users/{user}", [UserController::class, 'update'])->name('update');
+    Route::delete("/users", [UserController::class, 'destroy'])->name('destroy');
+});
+
+// Route::middleware('auth')->name('users.')->namespace('\App\Http\Controllers')->group(function () {
+//     Route::get('/users', 'UserController@index')->name('index');
+//     Route::get("/users{user}", [UserController::class, 'show'])->name('show');
+//     Route::post("/users", [UserController::class, 'store'])->name('store');
+//     Route::patch("/users/{user}", [UserController::class, 'update'])->name('update');
+//     Route::delete("/users", [UserController::class, 'destroy'])->name('destroy');
+// });
+
+?>
